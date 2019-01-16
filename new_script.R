7c0b9c483417e10b72e39899cab7801f76f363ff
Sara Contu <sara.contu@nhm.ac.uk>
2019-01-16 12:18:24 +0000

    added notes to the file

---
+++ new_script.R
@@ -0,0 +1,11 @@
+# in the git tab there are now the gitignore file, and 2 folder
+# the M mean that is already tracking the gitignore file
+# if we want to add a fiel we can tick the box on the Staged column
+
+# I am now adding this file to git
+
+
+# I want to try to add more to the file and add and coomit
+
+
+afsfsfsef
